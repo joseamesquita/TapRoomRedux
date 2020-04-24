76733882eabd2b0dd1f151a2f2174ec320d8fc5a
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-24 13:41:16 -0700

    add Keg component with Keg info

---
+++ src/Keg.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Keg(props) {
+  return (
+    <React.Fragment>
+      <div onClick={() => props.whenKegClicked(props.is)}>
+        <h3>{props.name}</h3>
+        <h3>{props.brand}</h3>
+        <h3>{props.price}</h3>
+        <h3>{props.flavor}</h3>
+      </div>
+    </React.Fragment>
+  );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  price: PropTypes.number,
+  flavor: PropTypes.string
+};
+
+export default Keg;
