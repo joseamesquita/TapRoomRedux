9c6e501568c666a45d112ef3682df9952cb58ba6
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-26 22:54:04 -0700

    inserted reusableform tag inside the return statement to display keg form

--- src/NewKegForm.js
+++ src/NewKegForm.js
@@ -1,10 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
 
 function NewKegForm(props) {
   function handleNewKegFormSubmission(event) {
     event.preventDefault();
-    props.onNewKegCreation({ name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, flavor: event.target.flavor.value })
+    props.onNewKegCreation({
+      name: event.target.name.value,
+      brand: event.target.brand.value,
+      price: event.target.price.value,
+      flavor: event.target.flavor.value
+    })
   }
 
   NewKegForm.propTypes = {
@@ -13,8 +19,9 @@ function NewKegForm(props) {
 
   return (
     <React.Fragment>
-      formSubmissionHandler={handleNewKegFormSubmission}
-      buttonText = "Submit Keg Request"
+      <ReusableForm
+        formSubmissionHandler={handleNewKegFormSubmission}
+        buttonText="Submit Keg Request" />
     </React.Fragment>
   )
 }
