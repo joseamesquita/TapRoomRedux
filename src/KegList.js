5a6fe7fc91c11bf98ac9247d97650ac6c59739a3
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-27 00:17:44 -0700

    modify keglist and refactor

--- src/KegList.js
+++ src/KegList.js
@@ -9,6 +9,7 @@ function KegList(props) {
       {props.kegList.map((keg) =>
         <Keg
           whenKegClicked={props.onKegSelection}
+          whenKegSellClicked={props.onKegSellClicked}
           name={keg.name}
           brand={keg.brand}
           price={keg.price}
