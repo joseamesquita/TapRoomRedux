a8ce5d550ca79539394878691ed1919308ec2802
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-26 22:31:49 -0700

    refactor / fixed bugs

--- src/Keg.js
+++ src/Keg.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 function Keg(props) {
   return (
     <React.Fragment>
-      <div onClick={() => props.whenKegClicked(props.is)}>
+      <div onClick={() => props.whenKegClicked(props.id)}>
         <h3>{props.name}</h3>
         <h3>{props.brand}</h3>
         <h3>{props.price}</h3>
