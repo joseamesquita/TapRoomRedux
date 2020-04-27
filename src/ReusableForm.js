59b71e74488951b1a3604a92c025beb8485b9005
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-26 21:11:20 -0700

    add reusableform to create input submissions

---
+++ src/ReusableForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop=types";
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type="text"
+          name="name"
+          placeholder="name of Root Beer" />
+        <input
+          type="text"
+          name="brand"
+          placeholder="brand of Root Beer" />
+        <input
+          type="text"
+          name="price"
+          placeholder="price of Root Beer" />
+        <input
+          type
+          name="flavor"
+          placeholder="flavor of Root Beer" />
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  )
+}
+
+ReusableForm.propTypes = {
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
