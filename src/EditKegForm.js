5a1e98d863e8a2c6411fd472ccf7fcd01774f938
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-26 21:23:35 -0700

    modify editKegForm to function

---
+++ src/EditKegForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReusableFrom from "./ReusableForm";
+import PropTypes from "prop-types";
+import { v4 } from "uuid";
+
+function EditKegForm(props) {
+  const { keg } = props;
+
+  function handleEditKegFormSubmission(event) {
+    event.preventDefault();
+    props.onEditKeg({
+      brand: event.target.type.value,
+      name: event.target.name.value,
+      price: event.target.price.value,
+      flavor: event.target.flavor.value,
+      id: v4()
+    })
+  }
+
+  return (
+    <React.Fragment>
+      formSubmissionHandler = {handleEditKegFormSubmission}
+      buttonText = "Update Keg" />
+    </React.Fragment>
+  );
+}
+
+EditKegForm.proptypes = {
+  onEditKeg: PropTypes.func
+};
+
+export default EditKegForm;
