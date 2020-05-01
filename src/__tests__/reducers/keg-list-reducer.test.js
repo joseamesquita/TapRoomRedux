56cf63015803ea9c68fd905afd2848558e82f836
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-01 11:18:11 -0700

    write test for adding kegs, fails test as expected

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,42 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+
+  let action;
+  const kegData = {
+    name: "Root Beer",
+    brand: "A&W's",
+    price: "$32",
+    flavor: "Rocky Road",
+    quantity: "64",
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new keg data to masterKegList', () => {
+    const { name, brand, price, flavor, quantity, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      flavor: flavor,
+      quantity: quantity,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        flavor: flavor,
+        quantity: quantity,
+        id: id
+      }
+    });
   });
 });
