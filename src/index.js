391ebeda7762095494f8df272830c3c244d9a847
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-01 13:53:37 -0700

    create redux store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers/keg-list-reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
