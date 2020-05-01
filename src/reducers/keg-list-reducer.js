dc1c5a5cacead05c54d0775d99b43dbac607da8c
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-01 13:09:51 -0700

    adding logic to add new keg data to the masterKegList, passes test

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_KEG':
+      const { name, brand, price, flavor, quantity, id } = action;
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          price: price,
+          flavor: flavor,
+          quantity: quantity,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 };
